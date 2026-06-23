0994cd4871e44ef88c1be1a0f439e8775c384876
seon0 <tjsdud00sy@gmail.com>
2026-06-23 12:25:03 +0900

    branch_test에서 수정

--- git_java/src/git_java/Test.java
+++ git_java/src/git_java/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test1");
-		System.out.println("Test2");
+		System.out.println("Test4");
 		
 	}
 
