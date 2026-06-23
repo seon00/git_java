b08d0700e6f11ff21520e8be6c44893b912642ad
seon0 <tjsdud00sy@gmail.com>
2026-06-23 12:46:50 +0900

    'pull하고 커밋'

--- git_java/src/git_java/Test.java
+++ git_java/src/git_java/Test.java
@@ -5,7 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test1");
-		System.out.println("Test4");
+		System.out.println("Test3");
+		// 작업 -> 스테이징 -> 커밋 -> 푸시 -> 충돌 -> 푸시
+		// Pull하고 test 하는 중 입니다.
 
 		
 	}
